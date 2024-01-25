7715f0e0f97c66270afc2f1d3acdd9c13f3ad075
OpenSourceDevlopment <157737849+OpenSourceDevlopment@users.noreply.github.com>
2024-01-25 23:47:01 +0530

    Create script.js

---
+++ Js/script.js
@@ -0,0 +1,4 @@
+function startGame() {
+    alert("Embark on your MindForge journey! Solve unique puzzles and unlock the mysteries of your mind.");
+    // Add logic to navigate to different levels and puzzles.
+}
